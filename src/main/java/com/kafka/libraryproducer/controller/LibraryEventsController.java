2e277d5a0a7ab71904dafaa336591c6a4e26b4a3
c.misman <mismancelaleddin@gmail.com>
2023-07-26 07:32:44 +0300

    sync or async message sending examples added

--- src/main/java/com/kafka/libraryproducer/controller/LibraryEventsController.java
+++ src/main/java/com/kafka/libraryproducer/controller/LibraryEventsController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 @Slf4j
 @RestController
 @RequestMapping("api/v1")
@@ -24,13 +27,25 @@ public class LibraryEventsController {
 
     @PostMapping("library-event")
     public ResponseEntity<LibraryEvent> createLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
-        log.info("library event : {} ", libraryEvent);
         try {
+            log.info("library event : {} ", libraryEvent);
             libraryEventsProducer.sendLibraryEvent(libraryEvent);
+            log.info("After sending library event : ");
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        log.info("After sending library event : ");
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PostMapping("library-event-2")
+    public ResponseEntity<LibraryEvent> createLibraryEvent2(@RequestBody LibraryEvent libraryEvent) {
+        try {
+            log.info("library event : {} ", libraryEvent);
+            libraryEventsProducer.sendLibraryEventApproach2(libraryEvent);
+            log.info("After sending approach2 library event : ");
+        } catch (JsonProcessingException | ExecutionException | InterruptedException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
--- src/main/java/com/kafka/libraryproducer/producer/LibraryEventsProducer.java
+++ src/main/java/com/kafka/libraryproducer/producer/LibraryEventsProducer.java
@@ -8,10 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @Service
@@ -38,6 +40,16 @@ public class LibraryEventsProducer {
                 handleFailure(key, value, throwable);
             }
         });
+    }
+
+    public SendResult<Integer, String> sendLibraryEventApproach2(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        Integer key = libraryEvent.libraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        // blocking call - get metadata about the kafka cluster
+        SendResult<Integer, String> sendResult = kafkaTemplate.send(topic, key, value).get(3, TimeUnit.SECONDS);
+        handleSuccess(key, value, sendResult);
+        return sendResult;
     }
 
     private void handleSuccess(Integer key, String value, SendResult<Integer, String> sendResult) {
