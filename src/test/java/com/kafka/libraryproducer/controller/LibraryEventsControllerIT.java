dd78d0397fcdf3d63742dc0a33473d5cdff6a7d8
c.misman <mismancelaleddin@gmail.com>
2023-08-10 15:15:22 +0300

    kafka broker with embedded server and test it

--- src/test/java/com/kafka/libraryproducer/controller/LibraryEventsControllerIT.java
+++ src/test/java/com/kafka/libraryproducer/controller/LibraryEventsControllerIT.java
@@ -8,9 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
-@ActiveProfiles("local")
+@EmbeddedKafka(topics = "library-events")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LibraryEventsControllerIT {
 
@@ -23,6 +23,32 @@ class LibraryEventsControllerIT {
         httpHeaders.set("Content-type", MediaType.APPLICATION_JSON_VALUE);
         HttpEntity<LibraryEvent> httpEntity = new HttpEntity<>(TestUtil.libraryEventRecord(), httpHeaders);
         ResponseEntity<LibraryEvent> responseEntity = restTemplate.exchange("/api/v1/library-event", HttpMethod.POST,
+                httpEntity, LibraryEvent.class);
+
+        Assertions.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+
+    }
+
+    @Test
+    void createLibraryEvent2() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Content-type", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<LibraryEvent> httpEntity = new HttpEntity<>(TestUtil.libraryEventRecord(), httpHeaders);
+        ResponseEntity<LibraryEvent> responseEntity = restTemplate.exchange("/api/v1/library-event-2", HttpMethod.POST,
+                httpEntity, LibraryEvent.class);
+
+        Assertions.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+
+    }
+
+    @Test
+    void createLibraryEvent3() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Content-type", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<LibraryEvent> httpEntity = new HttpEntity<>(TestUtil.libraryEventRecord(), httpHeaders);
+        ResponseEntity<LibraryEvent> responseEntity = restTemplate.exchange("/api/v1/library-event-3", HttpMethod.POST,
                 httpEntity, LibraryEvent.class);
 
         Assertions.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
